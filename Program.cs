2684cc5b2b345190a2662a859cfffe2a6d1f14f6
MariliaThomaz <79291965+MariliaThomaz@users.noreply.github.com>
2021-04-08 14:40:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _012_Lupe_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //For= Para
+            // while  = FAÇO Enquanto
+            // do while = faça enquanto
+            //foreach = Para cada
+
+            //Vetor que  lista mute valorada  ou que C# Fala em Areio
+            // veremosa eras Areio junto  estrura de repetição 
+
+           // for ()   // tem que ser feito   criterio de  parda  definido                                           // aqui declando a estrutura e ela de repetição
+                    // estes são lupes clasucos                                                                    // o que ela vai fazer? 
+                    // este lupe tem que twe criterio de parada                                                    // ela repeti tudo do abre  fecha chaves {}
+                    // para definir esta parada  cia varivelde controle                                           // até que condição para dela seja alçanda
+                    // este varivl ela funcioa como contador                                                      // estrura repetição ela paraaté quando o  criterio seja satifeito
+                    
+                
+           // { }                                                                                                // Motivo da estrutura de repetição é 
+                                                                                                                // repetir.
+                                                                                                               // Repetir um trejo de codio até que até uma condição 
+                                                                                                                  //seja satifeita.
+                                                                                                                 // *********************For************
+                                                                                                                //ele sempre vai repetir dentro mesmo laço
+            // Cria uma varivem contadora                                                                                                    //desntro do {}
+           // vairivel contole é  sempre tipo Int e nome dela ai I e voce igala a =0   
+           // aposa ;  vai definr para do lupe, ou voce definir quastas vese mostrar Hello World!
+           //criterio de para  i<5
+           // meu i começa  com  0 para faz a contem eu  declara I++
+           Console.WriteLine("comecou progama");
+               // Controle        //PARADA      // Incremeto 
+            for ( int i =0;      i < 5;              i++   )
+
+            {
+
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Valoe da vaiee I quantas veses ela repete: " +i);
+
+                //i++ se voce cola ele dentro do laço bagunça tudo
+            }
+            Console.WriteLine("Fim do progama");
+
+
+            // Resultado que ele vai moetrar Hello World! são 5 vese mas  a para é menor que 5?
+            // ele  conta  o zer "0"
+
+
+            // É esturura repetição ela tem começo e fim, muito bem definido
+            // for temcarateristica  de redia mas curta
+
+            // Poque I e devido usr ETRUTURA DE REPETIÇÃO em Vetor poisa no verto tem index por sso usa 
+            // não nesario que presiaria ser i
+
+
+            Console.ReadKey();
+        }
+    }
+}
