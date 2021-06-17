4a8ab2f58c4b61bee256dd702196f8f5d050ed03
MariliaThomaz <79291965+MariliaThomaz@users.noreply.github.com>
2021-06-17 17:35:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _021_Arrays_13_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
